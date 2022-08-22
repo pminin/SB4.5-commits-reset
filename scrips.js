c3061f1dd3cefb36c356bbe5d67bbf1b3976ec90
pminin <psminin@ya.ru>
2022-08-22 18:08:50 +0300

    added comment to scripts.js

--- scrips.js
+++ scrips.js
@@ -1,6 +1,7 @@
 (function(){
     {
-    //I don't know js, otherwise I would write a code not a comment   
+    //I don't know js, otherwise I would write a code not a comment
+    //comment for scripts.js SB 4.5   
     }
     //some code
 })();
